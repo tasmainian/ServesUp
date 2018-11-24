348eb197d4d2428f5796fcd1ac34b15677ae4c9a
Tasmiha Hassan <htasmiha@gmail.com>
2018-11-24 02:07:18 -0500

    Importing Bootstraps

--- serves-up/src/App.js
+++ serves-up/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   render() {
