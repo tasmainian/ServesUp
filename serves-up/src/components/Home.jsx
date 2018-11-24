9437d9b5c593c9a4080832f794e0729062f28065
Tasmiha Hassan <htasmiha@gmail.com>
2018-11-24 09:14:31 -0500

    Attempt for basic Layout

--- serves-up/src/components/Home.jsx
+++ serves-up/src/components/Home.jsx
@@ -7,18 +7,6 @@ export default class Home extends Component {
   render() {
     return (
     <div id="parent">
-      <div class = "Jumbotron">
-        <Jumbotron>
-          <h1>Serves Up</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
-          <p>
-            <Button bsStyle="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
-      </div>
       <div>
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
           <div class="container">
@@ -46,7 +34,7 @@ export default class Home extends Component {
       <header class="masthead">
       <div class="container d-flex h-100 align-items-center">
         <div class="mx-auto text-center">
-          <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
+          <h1 class="mx-auto my-0 text-uppercase">Serves Up</h1>
           <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
           <a href="#about" class="btn btn-primary js-scroll-trigger">Get Started</a>
         </div>
