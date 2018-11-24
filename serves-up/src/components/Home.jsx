09fcf6a7ea0f89e9163101de5dc0340cddff73e0
Tasmiha Hassan <htasmiha@gmail.com>
2018-11-24 14:16:45 -0500

    Working Form

--- serves-up/src/components/Home.jsx
+++ serves-up/src/components/Home.jsx
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import { Jumbotron, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css';
 import Header from "./Header";
+import Form from "./Form"
+
 
 export default class Home extends Component {
+
   render() {
     return (
     <div id="parent">
       <Header />
-      <Button />
+      <Form />
     </div>
     )
   }
