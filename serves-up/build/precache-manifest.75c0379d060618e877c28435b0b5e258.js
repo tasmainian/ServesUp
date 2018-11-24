578d3581296afa6c28f2b2ab626bbd90d8e69796
Tasmiha Hassan <htasmiha@gmail.com>
2018-11-24 01:53:19 -0500

    Production Build

---
+++ serves-up/build/precache-manifest.75c0379d060618e877c28435b0b5e258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e50f4d2e890802e82c8",
+    "url": "/static/css/main.42daa62d.chunk.css"
+  },
+  {
+    "revision": "7e50f4d2e890802e82c8",
+    "url": "/static/js/main.7e50f4d2.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "772becbb7fc50266b3523c9ac087dc88",
+    "url": "/index.html"
+  }
+];
