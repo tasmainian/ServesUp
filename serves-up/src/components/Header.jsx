7ff09dba044c708ed0224e146a9418b38fc7f735
Tasmiha Hassan <htasmiha@gmail.com>
2018-11-24 21:17:14 -0500

    Animations

--- serves-up/src/components/Header.jsx
+++ serves-up/src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { Jumbotron, Row, Col, Image, Button } from 'react-bootstrap';
+import {bounce} from 'animate.css';
 import './Home.css';
 
 export default class Header extends Component {
   render() {
     return (
     <div id="parent">
-    <div class="sandbox sandbox-correct-pronounciation">
-      <h1 class="heading heading-correct-pronounciation">
-        <em>Serves Up</em>
+    <div className="sandbox sandbox-correct-pronounciation">
+      <h1 className="heading heading-correct-pronounciation">
+
+        <em className="animated fadeIn delay-1s">Serves Up</em>
         <br />
-        <span>Let the restaurants find you.</span>
+          <span>Let the restaurants find you.</span>
       </h1>
     </div>
-
     </div>
     )
   }
