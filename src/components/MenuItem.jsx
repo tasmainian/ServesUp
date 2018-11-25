e8841693406f935f0d948e191e90b898bc826c9e
Tasmiha Hassan <htasmiha@gmail.com>
2018-11-25 07:39:23 -0500

    Cleaned up results

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import './menu-item.css';
+import './menu-item.scss';
+import {bounce} from 'animate.css';
 
 export default class MenuItem extends React.Component {
 
     render() {
         return (
-            <div className="menu-item">
-                <p className="menu-item__name">{this.props.itemName}</p>
-                <p className="menu-item__restaurant">{this.props.restaurantName}</p>
-                <p className="menu-item__calories">{this.props.calories + ' Cal'}</p>
+          <div>
+            <div className="menu-item animated slideInRight delay-1s">
+                <h6>Menu Item</h6>
+                <h2 className="menu-item__name">{this.props.itemName}</h2>
+            </div>
+            <div className="restaurants animated slideInRight delay-2s">
+                <h6>Restaurant</h6>
+                <h5 className="menu-item__restaurant">{this.props.restaurantName}</h5>
+            </div>
+            <div className="calories animated slideInRight delay-3s">
+                <h6>Calories</h6>
+                <h5 className="menu-item__calories">{this.props.calories + ' Cal'}</h5>
+            </div>
+            <div className="tags animated slideInRight delay-4s">
+                <h6>Tags</h6>
                 <div className="menu-item__tags">
-                    {this.props.tags.map((tag, index) => <p key={index} className="menu-item__tag">{tag}</p>)}
+                    {this.props.tags.map((tag, index) => <h5 key={index} className="menu-item__tag">{tag}</h5>)}
                 </div>
+            </div>
+                <br />
+                <br />
             </div>
         );
     }
